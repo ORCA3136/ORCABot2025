802a15e528cb4cb7c35fc9ae541aae96d5aa551b
Fletcher <fjbruffy@gmail.com>
2025-02-02 12:10:24 -0500

    Attempting to update the LED part of the constants file for the uphteenth time

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -181,20 +181,31 @@ public final class Constants {
 
     //Patterns 
     public static final double Rainbow = -0.99;
-    
+    240376
 
     //Basic colors
+    public static final double Hot_Pink = 0.57;
+    public static final double Dark_Red = 0.59;
     public static final double Red = 0.61;
+    public static final double Red_Orange = 0.63;
     public static final double Orange = 0.65;
+    public static final double Gold = 0.67;
     public static final double Yellow = 0.69;
+    public static final double Lawn_Green = 0.71;
     public static final double Lime = 0.73;
+    public static final double Dark_Green = 0.75;
     public static final double Green = 0.77;
+    public static final double Blue_Green = 0.79;
     public static final double Aqua = 0.81;
+    public static final double Sky_Blue = 0.83;
+    public static final double Dark_Blue = 0.85;
     public static final double Blue = 0.87;
+    public static final double Blue_Violet = 0.89;
     public static final double Violet = 0.91;
     public static final double White = 0.93;
     public static final double Gray = 0.95;
-    public static final double Black - 0.99;
+    public static final double Dark_Gray = 0.97;
+    public static final double Black = 0.99;
   }
  
 }
