c3e50975a1f403528f87a5926576419ca43d2619
Mac Fraser <macgregorfraser@yahoo.com>
2025-03-15 13:02:01 -0700

    Initial commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import java.io.File;
 
-import frc.robot.subsystems.SwerveSubsystem;
 
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.RobotBase;
@@ -24,13 +23,14 @@ import swervelib.SwerveInputStream;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  
 
-  // private final SwerveSubsystem driveBase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/ORCA2025"));
-  private final SwerveSubsystem driveBase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/ORCA2025"));
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    
 
   }
 
@@ -41,20 +41,18 @@ public class RobotContainer {
                                             .deadband(0.1)
                                             .scaleTranslation(0.8)
                                             .allianceRelativeControl(true);
+
   Command driveFieldOrientedAngularVelocity = driveBase.driveFieldOriented(driveAngularVelocity);
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
+
     driveBase.setDefaultCommand(driveFieldOrientedAngularVelocity);
+
   }
+
+
+
+
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -62,7 +60,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
     return null;
   }
 
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -4,41 +4,35 @@
 
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Meter;
-
-import java.io.File;
-
-import com.google.flatbuffers.Constants;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import swervelib.SwerveDrive;
-import swervelib.parser.SwerveParser;
 
-public class SwerveSubsystem extends SubsystemBase {
-  /** Creates a new SwerveSubsystem. */
-  SwerveDrive swerveDrive;
+public class swerveSubsystem extends SubsystemBase {
+  /** Creates a new swerveSubsystem. */
+  public swerveSubsystem() {}
 
-  
-
-  public SwerveSubsystem(File directory) {
-
-    try {
-     swerveDrive = new SwerveParser(directory).createSwerveDrive(4.0,  // Put in constants
-                                                                  new Pose2d(new Translation2d(Meter.of(0),
-                                                                                               Meter.of(0)),
-                                                                             Rotation2d.fromDegrees(0)));
-    } catch (Exception e)
-    {
-      throw new RuntimeException(e);
-    }
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
   }
 
-  public SwerveDrive getSwerveDrive(){
-    return swerveDrive;
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
   }
 
   @Override
